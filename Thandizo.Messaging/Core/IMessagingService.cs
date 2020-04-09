aeb2040c8a4677c449637d7321fa6697d783ca1f
Henry Alfred Sauzande <henry.sauzande@angledimension.com>
2020-04-09 12:04:52 +0200

    Added messaging service contract

---
+++ Thandizo.Messaging/Core/IMessagingService.cs
@@ -0,0 +1,11 @@
+﻿using System.Threading.Tasks;
+using Thandizo.DataModels.General;
+using Thandizo.DataModels.Messaging;
+
+namespace Thandizo.Messaging.Core
+{
+    public interface IMessagingService
+    {
+        Task<OutputResponse> SendMessage(MessageModel message);
+    }
+}
