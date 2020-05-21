765caa4e6d1396b13191a0bf0ed427f6fd0db749
Sokwani Munthali <sokwani.munthali@angledimension.com>
2020-05-21 15:28:23 +0200

    updated package raferences and changed project type from 2.0 to 2.1

--- Thandizo.Messaging/Email/EmailMessagingService.cs
+++ Thandizo.Messaging/Email/EmailMessagingService.cs
@@ -1,8 +1,5 @@
 ﻿using SocketLabs.InjectionApi;
 using SocketLabs.InjectionApi.Message;
-using System;
-using System.Collections.Generic;
-using System.Text;
 using System.Threading.Tasks;
 using Thandizo.DataModels.General;
 using Thandizo.DataModels.Messaging;
